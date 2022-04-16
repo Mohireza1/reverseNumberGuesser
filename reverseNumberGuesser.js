f4cc1fd3a2ac56f5bb93f6f68f5e0bb67a289004
Mohireza <mohireza50@gmail.com>
2022-04-16 11:42:12 +0430

    now style renders before the script does

--- reverseNumberGuesser.js
+++ reverseNumberGuesser.js
@@ -1,30 +1,30 @@
 "use strict";
-let firstNumber = 1;
-let lastNumber = 100;
-let midNumber = Math.floor(lastNumber / 2);
-let turns = 0;
-alert(`Pick a number between ${firstNumber} and ${lastNumber}`);
-while (lastNumber - firstNumber > 0) {
-  let size = prompt(
-    `Is your number higher or lower than ${midNumber} or a bingo?`
-  );
-  if (size === "lower") {
-    lastNumber = midNumber;
-    midNumber = Math.round((lastNumber + firstNumber) / 2);
-    turns++;
-  } else if (size === "higher") {
-    firstNumber = midNumber;
-    midNumber = Math.round((lastNumber + firstNumber) / 2);
-    turns++;
-  } else if (size === "bingo") {
-    alert(`Your number is ${midNumber}
-    (and it took me ${turns} turns to guess your number!)`);
-    break;
-  } else if (size === "cancel") {
-    break;
-  } else {
-    size = prompt(
-      `Is your number higher or lower than ${midNumber} or a bingo? (Please enter either "higher" or "lower" or "bingo")`
+setTimeout(() => {
+  let firstNumber = 1;
+  let lastNumber = 100;
+  let midNumber = Math.floor(lastNumber / 2);
+  let turns = 0;
+  alert(`Pick a number between ${firstNumber} and ${lastNumber}`);
+  while (lastNumber - firstNumber > 0) {
+    let size = prompt(
+      `Is your number higher or lower than ${midNumber} or a bingo?`
     );
+    if (size === "lower") {
+      lastNumber = midNumber;
+      midNumber = Math.round((lastNumber + firstNumber) / 2);
+      turns++;
+    } else if (size === "higher") {
+      firstNumber = midNumber;
+      midNumber = Math.round((lastNumber + firstNumber) / 2);
+      turns++;
+    } else if (size === "bingo") {
+      alert(`Your number is ${midNumber}
+(and it took me ${turns} turns to guess your number!)`);
+      break;
+    } else if (size === "cancel") {
+      break;
+    } else {
+      alert(`Please enter either "higher" or "lower" or "bingo "`);
+    }
   }
-}
+}, 20);
